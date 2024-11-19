d252a313c9030ef662ddfbedb585c31f28dbe9a4
root <root@6e149f2ab130>
2024-11-19 13:44:43 +0530

    Pushed Successfully

---
+++ test.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+import pytest
+from pages.login_page import LoginPage
+
+driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+driver.find_element(By.XPATH,'//input[@name="username"]').send_keys("Admin")
+driver.find_element(By.XPATH,'//input[@name="password"]').send_keys("admin123")
+time.sleep(5)
+   

d0c67d41f77fc235313609126a8b82178daf8ea1
root <root@6e149f2ab130>
2024-11-19 15:18:12 +0530

    Pushed Successfully

---
+++ locators/Login_page_locator.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+
+click_username=By.XPATH,'//input[@name="username"]'
+click_password=By.XPATH,'//input[@name="password"]'
+click_login_button=By.XPATH,'//button[@class="oxd-button oxd-button--medium oxd-button--main orangehrm-login-button"]'
